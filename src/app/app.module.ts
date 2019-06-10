e9d598368b35f09891f7adf191eb8d7c592a709a
466548 <tsuka.as@gmail.com>
2019-06-10 16:24:16 +0900

    change app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InputComponent } from './input/input.component';
+import { LabelComponent } from './label/label.component';
+import { MenuComponent } from './container/nav/menu/menu.component';
+import { NavComponent } from './container/nav/nav.component';
+import { LogoComponent } from './container/nav/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    InputComponent,
+    LabelComponent,
+    MenuComponent,
+    NavComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
